ec6af95a7e5194300b8969e0d3d85125738791af
Alain Herve <alainQtec@gmail.com>
2023-04-24 14:23:11 +0200

    Create index.php

---
+++ Practice_Projects/Simple_blog/index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>A simple blog system</title>
+</head>
+
+<body>
+    <?php
+    echo "This project could includes functionality for creating, editing, and deleting blog posts, as well as a commenting system and user authentication.";
+    ?>
+</body>
+
+</html>
