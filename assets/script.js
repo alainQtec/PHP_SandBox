63e7f08556d4a6d1593426a32feb4dee2951ed29
Alain Herve <alainQtec@gmail.com>
2023-04-26 10:47:06 +0200

    Update script.js
    Added some js to animate the php mascot

--- assets/script.js
+++ assets/script.js
@@ -1,29 +1,3 @@
-// Define canvas element and context
-var canvas = document.createElement('canvas');
-var ctx = canvas.getContext('2d');
-
-// Set canvas size to fill the screen
-canvas.width = window.innerWidth;
-canvas.height = window.innerHeight;
-
-// Add canvas to the page
-document.getElementById('canvas-container').appendChild(canvas);
-
-// Define word to animate
-var word = 'PHP SandBox';
-var fontSize = 120;
-var fontFamily = 'Arial';
-var textWidth = ctx.measureText(word).width;
-
-// Define particle animation variables
-var particleRadius = 2;
-var particleCount = 500;
-var particles = [];
-
-// Define mouse variables
-var mouseX = canvas.width / 2;
-var mouseY = canvas.height / 2;
-
 function cardClicked(card) {
     card.classList.add('card-clicked');
     setTimeout(() => {
@@ -33,85 +7,127 @@ function cardClicked(card) {
 
 function toggleDarkMode() {
     const body = document.querySelector('body');
-    body.classList.toggle('dark-mode');
+    body.classList.toggle('light-mode');
 }
-// Define animation loop
-function animate() {
-    // Clear canvas
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
+window.addEventListener('load', function () {
+    const canvas = document.getElementById('canvas1');
+    const ctx = canvas.getContext('2d');
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
 
-    // Draw word
-    ctx.font = fontSize + 'px ' + fontFamily;
-    ctx.fillStyle = 'white';
-    ctx.fillText(word, canvas.width / 2 - textWidth / 2, canvas.height / 2 + fontSize / 2);
-
-    // Update particle positions
-    particles.forEach(function (particle) {
-        particle.x += particle.vx;
-        particle.y += particle.vy;
-
-        // Check if particle is off screen
-        if (particle.x < 0 || particle.x > canvas.width || particle.y < 0 || particle.y > canvas.height) {
-            // Reset particle position and velocity
-            particle.x = canvas.width * Math.random();
-            particle.y = -10;
-            particle.vx = (Math.random() - 0.5) * 5;
-            particle.vy = Math.random() * 2;
+    class Particle {
+        constructor(effect, x, y, color) {
+            this.effect = effect;
+            this.x = this.originX = x;
+            this.y = this.originY = y;
+            this.size = 2;
+            this.color = color;
+            this.dx = 0;
+            this.dy = 0;
+            this.vx = 0;
+            this.vy = 0;
+            this.force = 0;
+            this.angle = 0;
+            this.distance = 0;
+            this.friction = 0.98;
+            this.ease = 0.2;
         }
-
-        // Draw particle
-        ctx.beginPath();
-        ctx.arc(particle.x, particle.y, particleRadius, 0, 2 * Math.PI, false);
-        ctx.fillStyle = 'white';
-        ctx.fill();
-    });
-
-    // Calculate distance from mouse to word
-    var distance = Math.sqrt(Math.pow(mouseX - canvas.width / 2, 2) + Math.pow(mouseY - canvas.height / 2, 2));
-
-    // If mouse is close enough to word, check for fast movement
-    if (distance < 100) {
-        canvas.style.cursor = 'pointer';
-        canvas.onclick = function () {
-            // Clear canvas and start particle animation
-            ctx.clearRect(0, 0, canvas.width, canvas.height);
-            particles = [];
-            for (var i = 0; i < particleCount; i++) {
-                particles.push({
-                    x: canvas.width * Math.random(),
-                    y: canvas.height * Math.random(),
-                    vx: (Math.random() - 0.5) * 5,
-                    vy: Math.random() * 2
-                });
+        update() {
+            this.dx = this.effect.mouse.x - this.x;
+            this.dy = this.effect.mouse.y - this.y;
+            this.distance = this.dx * this.dx + this.dy * this.dy;
+            this.force = -this.effect.mouse.radius / this.distance;
+            if (this.distance < this.effect.mouse.radius) {
+                this.angle = Math.atan2(this.dy, this.dx);
+                this.vx += this.force * Math.cos(this.angle);
+                this.vy += this.force * Math.sin(this.angle);
             }
-
-            // Display popup message
-            alert('Welcome to the PHP Sandbox!');
+            this.x += (this.vx *= this.friction) + (this.originX - this.x) * this.ease;
+            this.y += (this.vy *= this.friction) + (this.originY - this.y) * this.ease;
         }
-    } else {
-        canvas.style.cursor = 'default';
-        canvas.onclick = null;
     }
 
-    // Request next animation frame
-    requestAnimationFrame(animate);
-}
+    class Effect {
+        constructor(width, height) {
+            this.width = width;
+            this.height = height;
+            this.image = document.getElementById('canvasimage');
+            this.centerX = this.width / 2;
+            this.centerY = this.height / 2;
+            this.x = this.centerX - this.image.width / 2;
+            this.y = this.centerY - this.image.height / 2;
+            this.particles = [];
+            this.gap = 2;
+            this.mouse = {
+                radius: 5000,
+                x: this.centerX,
+                y: this.centerY
+            }
+            window.addEventListener("mousemove", event => {
+                this.mouse.x = event.x;
+                this.mouse.y = event.y;
+            });
 
-// Initialize particle positions
-for (var i = 0; i < particleCount; i++) {
-    particles.push({
-        x: canvas.width * Math.random(),
-        y: canvas.height * Math.random(),
-        vx: (Math.random() - 0.5) * 5,
-        vy: Math.random() * 2
-    });
-}
+            window.addEventListener("touchstart", event => {
+                this.mouse.x = event.changedTouches[0].clientX;
+                this.mouse.y = event.changedTouches[0].clientY;
+            }, false);
 
-// Add mousemove event listener to update mouse position
-document.addEventListener('mousemove', function (event) {
-    mouseX = event.clientX;
-    mouseY = event.clientY;
+            window.addEventListener("touchmove", event => {
+                event.preventDefault();
+                this.mouse.x = event.targetTouches[0].clientX;
+                this.mouse.y = event.targetTouches[0].clientY;
+            }, false);
+
+            window.addEventListener("touchend", event => {
+                event.preventDefault();
+                this.mouse.x = 0;
+                this.mouse.y = 0;
+            }, false);
+        }
+        init(context) {
+            context.drawImage(this.image, this.x, this.y);
+            var pixels = context.getImageData(0, 0, this.width, this.height).data;
+            var index;
+            for (var y = 0; y < this.height; y += this.gap) {
+                for (var x = 0; x < this.width; x += this.gap) {
+                    index = (y * this.width + x) * 4;
+                    const red = pixels[index];
+                    const green = pixels[index + 1];
+                    const blue = pixels[index + 2];
+                    const color = 'rgb(' + red + ',' + green + ',' + blue + ')';
+
+                    const alpha = pixels[index + 3];
+                    if (alpha > 0) {
+                        this.particles.push(new Particle(this, x, y, color));
+                    }
+                }
+            }
+            context.clearRect(0, 0, this.width, this.height);
+        }
+        update() {
+            for (var i = 0; i < this.particles.length; i++) {
+                this.particles[i].update();
+            }
+        }
+        render(context) {
+            context.clearRect(0, 0, this.width, this.height);
+            for (var i = 0; i < this.particles.length; i++) {
+                var p = this.particles[i];
+                context.fillStyle = p.color;
+                context.fillRect(p.x, p.y, p.size, p.size);
+            }
+        }
+    }
+
+    const effect = new Effect(canvas.width, canvas.height);
+    effect.init(ctx);
+
+    function animate() {
+        effect.update();
+        effect.render(ctx);
+        requestAnimationFrame(animate);
+    }
+    animate();
+
 });
-
-// Start animation loop
-requestAnimationFrame(animate);
