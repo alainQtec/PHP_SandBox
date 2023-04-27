9ece2213cc10e88637cb0b6f60c00b1c31784c7b
Alain Herve <alainQtec@gmail.com>
2023-04-27 19:29:09 +0200

    Create Install_wp.ps1

---
+++ Scripts/Install_wp.ps1
@@ -0,0 +1,19 @@
+﻿# .Synopsis
+#   Easy Wordpress Installer.
+
+# .Description
+#   Quick and Easy Download and Installation of WP
+# * Just place this file to the directory where you want to install wordpress
+# * and then browse the file (./installer.php) using any browser.
+# * The files will be downloaded and you will be moved to the setup page
+# .NOTES
+#   Place into the root folder where you want to install wordpress and
+#   Browse the file from any browser. It will download the latest wordpress and move you to the setup page.
+#   Isn't it just too easy?
+# Requirements:
+#   PHP 5 >= 5.2.0 or PHP 7. PHP needs to have the zip extension installed.
+param()
+$IsPHPinstalled = Get-Command php -ErrorAction Ignore
+if ($IsPHPinstalled) {
+    php ./Install_wp.php
+}
