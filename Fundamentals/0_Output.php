abf082034d5c31019e8676b1c358d6076bd67082
Alain Herve <alainQtec@gmail.com>
2023-05-02 02:51:01 +0200

    Update 0_Output.php

--- Fundamentals/0_Output.php
+++ Fundamentals/0_Output.php
@@ -1,3 +1,7 @@
+<?php
+session_start();
+// Outputs
+?>
 <!DOCTYPE html>
 <html lang="en">
 
@@ -33,7 +37,7 @@
         const resultDiv = document.querySelector('.result');
         button.addEventListener('click', () => {
             const xhr = new XMLHttpRequest();
-            xhr.open('GET', 'path/to/this/index.php');
+            xhr.open('GET', '<?php echo __FILE__ ?>');
             xhr.onload = () => {
                 resultDiv.innerHTML = xhr.responseText;
             };
@@ -41,7 +45,41 @@
         });
     </script>
     <?php //codeTorun
-    echo 'This is the result of my PHP code.';
+    /* ------- Outputting Content ------- */
+    /*
+    A .php file running on a server, can output both HTML and PHP code.
+    There are multiple functions that can be used to output data to the browser.
+    */
+    // Echo - Output strings, numbers, html, etc
+    echo 'Hello';
+    echo 123;
+    echo '<h1>Hello</h1>';
+
+    // print - Similar to echo, but a bit slower
+    print 'Hello';
+
+    // print_r - Gives a bit more info. Can be used to print arrays
+    print_r('Hello');
+    print_r([1, 2, 3]);
+
+    // var_dump - Even more info like data type and length
+    var_dump('Hello');
+    var_dump([1, 2, 3]);
+
+    // Escaping characters with a backslash
+    echo "Is your name O\'reilly?";
+
+    /* ------------ Comments ------------ */
+
+    // This is a single line comment
+
+    /*
+      * This is a multi-line comment
+      *
+      * It can be used to comment out a block of code
+      */
+
+    // If there is more content after the PHP, such as this file, you do need the ending tag. Otherwise you do not.
     ?>
 </body>
 
